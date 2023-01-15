43d19a6c208880090d313dbce24498427123670d
Üzeyir Cenker Arda <202104115040@dogus.edu.tr>
2023-01-15 16:34:45 +0300

    Yürüme & Zıplma
    yürüme ve zıplama kodları yazıldı kısmi şekilde çalışıyor bool düzeltilmeli !

---
+++ Tunanın Oyun/Assets/JumpAndMove.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpAndMove : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    private float moveSpeed;
+    private float jumpSpeed;
+    private bool isJumping;
+    private float moveH;
+    private float moveV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 2f;
+        jumpSpeed = 10f;
+        isJumping = false;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveH = Input.GetAxisRaw("Horizontal");
+        moveV = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        if(moveH > 0.1f || moveH < -0.1f) 
+        {
+            rb2D.AddForce(new Vector2(moveH * moveSpeed, 0),ForceMode2D.Impulse);
+        }
+        if (!isJumping && moveV > 0.1f )
+        {
+            rb2D.AddForce(new Vector2(0f,moveV* moveSpeed),ForceMode2D.Impulse);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isJumping = true;
+        }
+    }
+}
